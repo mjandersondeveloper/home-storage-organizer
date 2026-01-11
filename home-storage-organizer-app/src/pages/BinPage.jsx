32d34daac95d029f7804b6fc6aa460604d6ef20d
Marcus Anderson <manderson332@gatech.edu>
2026-01-10 23:47:44 -0500

    made a functional page

--- home-storage-organizer-app/src/pages/BinPage.jsx
+++ home-storage-organizer-app/src/pages/BinPage.jsx
@@ -1,12 +1,84 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getAllBins, saveAllBins } from "../api/binStorage";
 
 export default function BinPage() {
   const { binId } = useParams();
 
+  const [bins, setBins] = useState({});
+  const [bin, setBin] = useState(null);
+  const [newItem, setNewItem] = useState("");
+
+  useEffect(() => { 
+    async function fetchBins() {
+      const binData = await getAllBins();
+      setBins(binData);
+      setBin(binData[binId]);
+    }
+    fetchBins();
+  }, [binId]);
+
+  if (!bin) {
+    return <p>Bin not found!</p>
+  }
+
+  const addItem = async () => {
+    if (!newItem.trim()) {
+      return;
+    }
+
+    const updatedBin = {
+      ...bins,
+      [binId]: {
+        ...bin,
+        items: [...bin.items, newItem]
+      }
+    };
+
+    setBins(updatedBin);
+    setBin(updatedBin[binId]);
+    setNewItem("");
+
+    await saveAllBins(updatedBin);
+  }
+
+  const removeItem = async (index) => {
+    const updatedItems = bin.items.filter((_, i) => i !== index);
+    const updatedBin = {
+      ...bins,
+      [binId]: {
+        ...bin,
+        items: updatedItems
+      }
+    }
+
+    setBins(updatedBin);
+    setBin(updatedBin[binId]);
+
+    await saveAllBins(updatedBin);
+  };
+
   return (
     <div>
-      <h2>Bin: {binId}</h2>
-      <p>This is where bin contents will live.</p>
+      <h2>Bin: {bin.name}</h2>
+
+      <ul>
+        {bin.items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+
+       <div>
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          placeholder="Add item..."
+        />
+        <button onClick={addItem}>Add</button>
+      </div>
     </div>
   );
 }
